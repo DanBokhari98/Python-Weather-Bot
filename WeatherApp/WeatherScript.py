928f1372b9b44a25884b5bc1a8c3e515a3419ea2
danbokhari98 <danbokhari98@gmail.com>
2020-03-04 22:10:56 -0500

    Changes to twitter app

--- WeatherApp/WeatherScript.py
+++ WeatherApp/WeatherScript.py
@@ -1,9 +1,11 @@
 import twitterAPI, openweatherAPI
 
-twitter = get_api()
-weather = printWeather()
+#twitter = get_api()
+weather = openweatherAPI.printWeather()
+print(weather)
 
 
 # Run script for 8:00Am everymorning
-#status = twitter.PostUpdate(weather)
-print(weather)
+status = twitter.PostUpdate(weather)
+
+#Finished 90% of weather app. 
--- WeatherApp/openweatherAPI.py
+++ WeatherApp/openweatherAPI.py
@@ -6,7 +6,13 @@
 import requests, json, sys, os
 
 # Enter your API key here
-api_key = "apikey"
+api_key = ""
+dict_desc = [{'clear sky': "Don't look at the sun."}, {'Rain':'Bring an umbrella!'}, {'Snow':'Time to build a snowman!'}]
+def desc_message(desc):
+    index = next((cond for cond, d in enumerate(dict_desc) if desc in d), -1)
+    if index == -1:
+        return ''
+    return dict_desc[index][desc]
 
 def get_weather(api_key, location):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, api_key)
@@ -17,11 +23,14 @@ def printWeather():
     weather = get_weather(api_key, 'New York')
     forcasts = weather['weather']
     temp = weather['main']
-    #cond = forcasts[0]['main']
+    temp_min = convert(temp['temp_min'])
+    temp_max = convert(temp['temp_max'])
     desc = forcasts[0]['description']
-    celc_to_fah = ( temp['temp']* (9/5))+32
-    the_weather = "Todays forecast is predicted to be {} at a temperature of {}ºF".format(desc, celc_to_fah)
+    print(desc)
+    celc_to_fah = convert(temp['temp'])
+    msg = desc_message(desc)
+    the_weather = "Todays forecast is predicted to be {} at a temperature of {}ºF with a min of {}ºF and a max of {}ºF. {}".format(desc, celc_to_fah, temp_min, temp_max,msg)
     return the_weather
 
-
-print(printWeather())
+def convert(temp):
+    return "{0:.0f}".format((temp * (9/5))+32)
